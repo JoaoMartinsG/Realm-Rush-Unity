9db920cd748ff3298dc7f255bbab6b929b382c70
Joao Martins <JoaoMGWork@hotmail.com>
2023-08-15 17:56:41 +0100

    Simplified Enemy mover new waypoint

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField]
-    List<Waypoint> path = new List<Waypoint>();
+    List<Waypoint> path = new();
 
     [SerializeField]
     [Range(0f, 5f)]
